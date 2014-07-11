73ff0c83aca8f6f7095fbb41567747044ffcb20f
Marconi Moreto <caketoad@gmail.com>
2014-07-12 06:33:57 +0800

    add decoder with string and integer support

--- bencode.go
+++ bencode.go
@@ -1,23 +1,89 @@
 package byteslego
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
 )
 
-type Bencoder struct{}
-
-func (b *Bencoder) EncStr(s string) string {
-	return fmt.Sprintf("%d:%s", len(s), s)
+type bencoder struct {
+	bufio.Writer
 }
 
-func (b *Bencoder) EncInt(i int64) string {
-	return fmt.Sprintf("i%de", i)
+func (b *bencoder) encode(d interface{}) (string, error) {
+	var (
+		out string
+		err error
+	)
+	switch d.(type) {
+	case int, int32, int64:
+		out, err = b.encodeInt(int64(d.(int)))
+	case string:
+		out, err = b.encodeStr(d.(string))
+	case []interface{}:
+		out, err = b.encodeList(d.([]interface{}))
+	case map[string]interface{}:
+		out, err = b.encodeDict(d.(map[string]interface{}))
+	}
+	return out, err
 }
 
-func (b *Bencoder) EncDict(d map[string]interface{}) string {
-	var encStrs []string
+func (b *bencoder) encodeInt(i int64) (string, error) {
+	return fmt.Sprintf("i%de", i), nil
+}
+
+func (b *bencoder) encodeStr(s string) (string, error) {
+	return fmt.Sprintf("%d:%s", len(s), s), nil
+}
+
+func (b *bencoder) encodeList(l []interface{}) (string, error) {
+	var (
+		s   []string
+		err error
+	)
+	for _, e := range l {
+		switch e.(type) {
+		case int, int32, int64:
+			i, err := b.encodeInt(int64(e.(int)))
+			if err != nil {
+				break
+			}
+			s = append(s, i)
+		case string:
+			es, err := b.encodeStr(e.(string))
+			if err != nil {
+				break
+			}
+			s = append(s, es)
+		case []interface{}:
+			el, err := b.encodeList(e.([]interface{}))
+			if err != nil {
+				break
+			}
+			s = append(s, el)
+		case map[string]interface{}:
+			ed, err := b.encodeDict(e.(map[string]interface{}))
+			if err != nil {
+				break
+			}
+			s = append(s, ed)
+		}
+	}
+
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("l%se", strings.Join(s, "")), nil
+}
+
+func (b *bencoder) encodeDict(d map[string]interface{}) (string, error) {
+	var (
+		s   []string
+		err error
+	)
 
 	// sort keys first
 	keys := make(sort.StringSlice, len(d))
@@ -30,85 +96,216 @@ func (b *Bencoder) EncDict(d map[string]interface{}) string {
 
 	for _, k := range keys {
 		v := d[k]
-		enckey := b.EncStr(k)
+		enckey, err := b.encodeStr(k)
+		if err != nil {
+			break
+		}
+
 		switch v.(type) {
 		case string:
-			encStrs = append(
-				encStrs,
-				fmt.Sprintf(
-					"%s%s",
-					enckey,
-					b.EncStr(v.(string)),
-				),
-			)
+			es, err := b.encodeStr(v.(string))
+			if err != nil {
+				break
+			}
+			s = append(s, fmt.Sprintf("%s%s", enckey, es))
 		case int, int32, int64:
-			encStrs = append(
-				encStrs,
-				fmt.Sprintf(
-					"%s%s",
-					enckey,
-					b.EncInt(int64(v.(int))),
-				),
-			)
+			ei, err := b.encodeInt(int64(v.(int)))
+			if err != nil {
+				break
+			}
+			s = append(s, fmt.Sprintf("%s%s", enckey, ei))
 		case []interface{}:
-			encStrs = append(
-				encStrs,
-				fmt.Sprintf(
-					"%s%s",
-					enckey,
-					b.EncList(v.([]interface{})),
-				),
-			)
+			el, err := b.encodeList(v.([]interface{}))
+			if err != nil {
+				break
+			}
+			s = append(s, fmt.Sprintf("%s%s", enckey, el))
+		case map[string]interface{}:
+			ed, err := b.encodeDict(v.(map[string]interface{}))
+			if err != nil {
+				break
+			}
+			s = append(s, fmt.Sprintf("%s%s", enckey, ed))
 		case map[string][]interface{}:
-			var sEncStrs []string
+			var ss []string
 			val := v.(map[string][]interface{})
 			for sk, sv := range val {
-				sEncStrs = append(
-					sEncStrs,
-					fmt.Sprintf(
-						"%s%s",
-						b.EncStr(sk),
-						b.EncList(sv),
-					),
-				)
+				ses, err := b.encodeStr(sk)
+				if err != nil {
+					break
+				}
+				sel, err := b.encodeList(sv)
+				if err != nil {
+					break
+				}
+				ss = append(ss, fmt.Sprintf("%s%s", ses, sel))
 			}
 
-			encStrs = append(
-				encStrs,
-				fmt.Sprintf(
-					"%sd%se",
-					enckey,
-					strings.Join(sEncStrs, ""),
-				),
-			)
-		case map[string]interface{}:
-			encStrs = append(
-				encStrs,
-				fmt.Sprintf(
-					"%s%s",
-					enckey,
-					b.EncDict(v.(map[string]interface{})),
-				),
-			)
+			if err != nil {
+				break
+			}
+			s = append(s, fmt.Sprintf("%sd%se", enckey, strings.Join(ss, "")))
 		}
 	}
 
-	return fmt.Sprintf("d%se", strings.Join(encStrs, ""))
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("d%se", strings.Join(s, "")), nil
 }
 
-func (b *Bencoder) EncList(l []interface{}) string {
-	var encStrs []string
-	for _, s := range l {
-		switch s.(type) {
-		case string:
-			encStrs = append(encStrs, b.EncStr(s.(string)))
-		case int, int32, int64:
-			encStrs = append(encStrs, b.EncInt(int64(s.(int))))
-		case []interface{}:
-			encStrs = append(encStrs, b.EncList(s.([]interface{})))
-		case map[string]interface{}:
-			encStrs = append(encStrs, b.EncDict(s.(map[string]interface{})))
+type bdecoder struct {
+	bufio.Reader
+}
+
+func (b *bdecoder) decode() (interface{}, error) {
+	c, err := b.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+
+	// we read first byte on start of the loop, so we unread it
+	// and read from the start up-to delimiter since the size
+	// can have more than 1 digit.
+	if err = b.UnreadByte(); err != nil {
+		return nil, err
+	}
+
+	var out interface{}
+	switch c {
+	case 'i':
+		// then delegate to decoding integer
+		out, err = b.decodeInt()
+		if err != nil {
+			break
+		}
+	case 'l':
+		out, err = b.decodeList()
+		if err != nil {
+			break
+		}
+
+		// decoding a list puts it in a wrapper list,
+		// so we extract that single item as the decoded list
+		out = out.([]interface{})[0]
+	case 'd':
+
+	// case 'e': -- bencoding cannot start in e
+
+	default:
+		out, err = b.decodeStr()
+		if err != nil {
+			break
 		}
 	}
-	return fmt.Sprintf("l%se", strings.Join(encStrs, ""))
+
+	return out, err
+}
+
+func (b *bdecoder) decodeStr() (string, error) {
+	rsize, err := b.ReadString(':')
+	if err != nil {
+		return "", err
+	}
+
+	// since rsize will include colon, we strip it
+	rsize = rsize[:len(rsize)-1]
+
+	size, err := strconv.Atoi(rsize)
+	if err != nil {
+		return "", err
+	}
+
+	// read the next bytes based on the size
+	var buf []byte
+	for i := 0; i < size; i++ {
+		rb, err := b.ReadByte()
+		if err != nil {
+			return "", err
+		}
+		buf = append(buf, rb)
+	}
+
+	return string(buf), nil
+}
+
+func (b *bdecoder) decodeInt() (int64, error) {
+	// read int together with its delimeters
+	rint, err := b.ReadString('e')
+	if err != nil {
+		return -1, err
+	}
+
+	// strip int delimeter and convert ti int64
+	i, err := strconv.ParseInt(rint[1:len(rint)-1], 10, 64)
+	if err != nil {
+		return -1, err
+	}
+
+	return i, nil
+}
+
+func (b *bdecoder) decodeList() ([]interface{}, error) {
+	var out []interface{}
+	for {
+		c, err := b.ReadByte()
+
+		// if nothing to read, return all we have
+		if err != nil {
+			return out, nil
+		}
+
+		switch c {
+		case 'i':
+			if err = b.UnreadByte(); err != nil {
+				return nil, err
+			}
+			i, err := b.decodeInt()
+			if err != nil {
+				break
+			}
+			out = append(out, i)
+		case 'l':
+			// if its a list, we don't unread so we can
+			// continue processing its items
+			l, err := b.decodeList()
+			if err != nil {
+				break
+			}
+			out = append(out, l)
+		case 'd':
+
+		case 'e':
+			return out, nil
+		default:
+			if err = b.UnreadByte(); err != nil {
+				return nil, err
+			}
+			s, err := b.decodeStr()
+			if err != nil {
+				break
+			}
+			out = append(out, s)
+		}
+
+		if err != nil {
+			return nil, err
+		}
+	}
+}
+
+func (b *bdecoder) decodeDict() (map[string]interface{}, error) {
+
+	return nil, nil
+}
+
+func Bencode(d interface{}) (string, error) {
+	e := &bencoder{}
+	return e.encode(d)
+}
+
+func Bdecode(s string) (interface{}, error) {
+	r := bufio.NewReader(bytes.NewBufferString(s))
+	d := bdecoder{*r}
+	return d.decode()
 }
--- bencode_test.go
+++ bencode_test.go
@@ -6,131 +6,152 @@ import (
 )
 
 func TestBencoder(t *testing.T) {
-	bencoder := &Bencoder{}
-
 	Convey("should encode string", t, func() {
-		So(bencoder.EncStr("hello"), ShouldEqual, "5:hello")
+		s, _ := Bencode("hello")
+		So(s, ShouldEqual, "5:hello")
 	})
 
 	Convey("should encode integer", t, func() {
-		So(bencoder.EncInt(28), ShouldEqual, "i28e")
+		s, _ := Bencode(28)
+		So(s, ShouldEqual, "i28e")
 	})
 
 	Convey("should encode dictionary", t, func() {
 		Convey("with string values", func() {
-			So(
-				bencoder.EncDict(map[string]interface{}{
-					"username": "bob",
-					"password": "secret",
-				}),
-				ShouldEqual,
-				"d8:password6:secret8:username3:bobe",
-			)
+			s, _ := Bencode(map[string]interface{}{
+				"username": "bob",
+				"password": "secret",
+			})
+			So(s, ShouldEqual, "d8:password6:secret8:username3:bobe")
 		})
 
 		Convey("with integer values", func() {
-			So(
-				bencoder.EncDict(map[string]interface{}{
-					"age":    30,
-					"number": 38,
-				}),
-				ShouldEqual,
-				"d3:agei30e6:numberi38ee",
-			)
+			s, _ := Bencode(map[string]interface{}{
+				"age":    30,
+				"number": 38,
+			})
+			So(s, ShouldEqual, "d3:agei30e6:numberi38ee")
 		})
 
 		Convey("with list values", func() {
-			So(
-				bencoder.EncDict(map[string]interface{}{
-					"fruits": []interface{}{"apple", "banana"},
-					"number": []interface{}{1, 2, 3},
-				}),
-				ShouldEqual,
-				"d6:fruitsl5:apple6:bananae6:numberli1ei2ei3eee",
-			)
+			s, _ := Bencode(map[string]interface{}{
+				"fruits": []interface{}{"apple", "banana"},
+				"number": []interface{}{1, 2, 3},
+			})
+			So(s, ShouldEqual, "d6:fruitsl5:apple6:bananae6:numberli1ei2ei3eee")
 		})
 
 		Convey("with dictionary values", func() {
-			So(
-				bencoder.EncDict(map[string]interface{}{
-					"collection": map[string]interface{}{
-						"fruit":  "apple",
-						"number": 1,
-					},
-				}),
-				ShouldEqual,
-				"d10:collectiond5:fruit5:apple6:numberi1eee",
-			)
+			s, _ := Bencode(map[string]interface{}{
+				"collection": map[string]interface{}{
+					"fruit":  "apple",
+					"number": 1,
+				},
+			})
+			So(s, ShouldEqual, "d10:collectiond5:fruit5:apple6:numberi1eee")
 		})
 
 		Convey("with mixed values", func() {
-			So(
-				bencoder.EncDict(map[string]interface{}{
-					"collection": map[string][]interface{}{
-						"fruits":  []interface{}{"apple", "banana"},
-						"numbers": []interface{}{1, 2, 3},
-					},
-				}),
-				ShouldEqual,
-				"d10:collectiond6:fruitsl5:apple6:bananae7:numbersli1ei2ei3eeee",
-			)
+			s, _ := Bencode(map[string]interface{}{
+				"collection": map[string][]interface{}{
+					"fruits":  []interface{}{"apple", "banana"},
+					"numbers": []interface{}{1, 2, 3},
+				},
+			})
+			So(s, ShouldEqual, "d10:collectiond6:fruitsl5:apple6:bananae7:numbersli1ei2ei3eeee")
 		})
 	})
 
 	Convey("should encode list", t, func() {
 		Convey("with strings", func() {
-			So(
-				bencoder.EncList([]interface{}{"apple", "banana"}),
-				ShouldEqual,
-				"l5:apple6:bananae",
-			)
+			s, _ := Bencode([]interface{}{"apple", "banana"})
+			So(s, ShouldEqual, "l5:apple6:bananae")
 		})
 
 		Convey("with integers", func() {
-			So(
-				bencoder.EncList([]interface{}{1, 2, 3}),
-				ShouldEqual,
-				"li1ei2ei3ee",
-			)
+			s, _ := Bencode([]interface{}{1, 2, 3})
+			So(s, ShouldEqual, "li1ei2ei3ee")
 		})
 
 		Convey("with list", func() {
-			So(
-				bencoder.EncList([]interface{}{
-					[]interface{}{"chocolate", 4},
-					[]interface{}{"candy", 5},
-				}),
-				ShouldEqual,
-				"ll9:chocolatei4eel5:candyi5eee",
-			)
+			s, _ := Bencode([]interface{}{
+				[]interface{}{"chocolate", 4},
+				[]interface{}{"candy", 5},
+			})
+			So(s, ShouldEqual, "ll9:chocolatei4eel5:candyi5eee")
 		})
 
 		Convey("with dictionary", func() {
-			So(
-				bencoder.EncList([]interface{}{
-					map[string]interface{}{
-						"color":  "red",
-						"number": 1,
-					},
-				}),
-				ShouldEqual,
-				"ld5:color3:red6:numberi1eee",
-			)
+			s, _ := Bencode([]interface{}{
+				map[string]interface{}{
+					"color":  "red",
+					"number": 1,
+				},
+			})
+			So(s, ShouldEqual, "ld5:color3:red6:numberi1eee")
 		})
 
 		Convey("with mixed", func() {
-			So(
-				bencoder.EncList([]interface{}{
-					map[string]interface{}{
-						"random": map[string]interface{}{
-							"colors": []interface{}{"red", "blue"},
-							"number": 1,
-						},
+			s, _ := Bencode([]interface{}{
+				map[string]interface{}{
+					"random": map[string]interface{}{
+						"colors": []interface{}{"red", "blue"},
+						"number": 1,
 					},
-				}),
-				ShouldEqual,
-				"ld6:randomd6:colorsl3:red4:bluee6:numberi1eeee",
-			)
+				},
+			})
+			So(s, ShouldEqual, "ld6:randomd6:colorsl3:red4:bluee6:numberi1eeee")
+		})
+	})
+}
+
+func TestBdecoder(t *testing.T) {
+	Convey("should decode string", t, func() {
+		Convey("valid string", func() {
+			s, _ := Bdecode("5:hello")
+			So(s, ShouldEqual, "hello")
+		})
+
+		Convey("string without size", func() {
+			_, err := Bdecode("hello")
+			So(err, ShouldNotEqual, nil)
+		})
+
+		Convey("invalid string size", func() {
+			_, err := Bdecode("10:hello")
+			So(err, ShouldNotEqual, nil)
+		})
+	})
+
+	Convey("should decode integer", t, func() {
+		Convey("valid integer", func() {
+			i, _ := Bdecode("i1e")
+			So(i, ShouldEqual, 1)
+		})
+
+		Convey("invalid integer", func() {
+			_, err := Bdecode("20")
+			So(err, ShouldNotEqual, nil)
+		})
+	})
+
+	Convey("should decode list", t, func() {
+		Convey("with integers", func() {
+			l, _ := Bdecode("li1ei2ei3ee")
+			So(l, ShouldResemble, []interface{}{int64(1), int64(2), int64(3)})
+		})
+
+		Convey("with strings", func() {
+			l, _ := Bdecode("l5:apple6:bananae")
+			So(l, ShouldResemble, []interface{}{"apple", "banana"})
+		})
+
+		Convey("with list", func() {
+			l, _ := Bdecode("ll9:chocolatei4eel5:candyi5eee")
+			So(l, ShouldResemble, []interface{}{
+				[]interface{}{"chocolate", int64(4)},
+				[]interface{}{"candy", int64(5)},
+			})
 		})
 	})
 }
